ff30e773b15c2d534a4c9aca918fffd0f3e85faa
ben leighton <ben.leighton@csiro.au>
2015-02-05 18:38:35 +1100

    cp file before trying to commit it

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 docker build -t test .
 docker run -v $DIR:/shared test /bin/bash -c "/shared/test.sh"  > test_result.txt 
-if [ -f ./last_test_result.txt ];
+if [ -e "./last_test_result.txt" ];
 then
 	if diff ./test_result.txt ./last_test_result.txt >/dev/null ; then
 	  echo "Success: results are reproduced" 
@@ -17,6 +17,7 @@ then
 	  exit 1 
 	fi
 else
+	cp test_result.txt last_test_result.txt 
         git add last_test_result.txt 
 	git commit -m "new result"
 	git push
